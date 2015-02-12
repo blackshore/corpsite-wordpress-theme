5d06139f36064f129b67850b9c409466a17e2aa7
davidmarquis <davidmarquis@me.com>
2015-02-12 10:27:50 -0500

    Initial import of element-navigation.php (identical as parent theme)

---
+++ theme-core/theme-elements/element-navigation.php
@@ -0,0 +1,25 @@
+<!-- DESKTOP MENU -->
+<div id="superfish-menu" class="dropdown-menu <?php echo ot_get_option("submenu_indicator_position"); ?>">
+
+    <!-- DEFAULT NAVIGATION -->
+    <?php
+    if (has_nav_menu('primary_menu')) {
+
+        wp_nav_menu( array(
+                'container' =>false,
+                'theme_location' => 'primary_menu',
+                'menu_class' => 'menu',
+                'echo' => true,
+                'before' => '',
+                'after' => '',
+                'link_before' => '',
+                'link_after' => '',
+                'depth' => 0,
+                'walker' => new description_walker())
+        );
+
+    }
+    ?>
+    <!-- /DEFAULT NAVIGATION -->
+</div>
+<!-- /DESKTOP MENU -->
